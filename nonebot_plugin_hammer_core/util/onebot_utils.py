98e7cc4b1f37addc57363e35688c1ce759e4b8c8
ArgonarioD <arnordorian@sina.com>
2022-08-22 11:35:28 +0800

    when target can be found in current group, get_qq_nickname_with_group() will no longer treat it as the member of another group

--- nonebot_plugin_hammer_core/util/onebot_utils.py
+++ nonebot_plugin_hammer_core/util/onebot_utils.py
@@ -13,9 +13,6 @@ async def get_qq_nickname_with_group(
 ) -> str:
     if target_group_id is None:
         target_group_id = current_group_id
-    if target_group_id != current_group_id: # if not the same group
-        info = await bot.get_stranger_info(user_id=user_id)
-        return await __stranger_convert(bot, info['nickname'], user_id, target_group_id, pattern, target_group_pattern)
 
     try:
         info = await bot.get_group_member_info(group_id=current_group_id, user_id=user_id)
@@ -23,18 +20,24 @@ async def get_qq_nickname_with_group(
         if len(info['card'].strip()) == 0:
             nickname = info['nickname']
         else:
-            nickname = info['card']
-        return pattern.replace('{target_group}','').replace('{nickname}', nickname).replace('{qq_id}', f'({user_id})')
+            nickname = info['card'].strip()
+        return pattern.replace('{target_group}', '').replace('{nickname}', nickname).replace('{qq_id}', f'({user_id})')
     except ActionFailed as exc:
         if exc.info['retcode'] == 100:
             # if not the same group
             info = await bot.get_stranger_info(user_id=user_id)
-            return await __stranger_convert(bot, info['nickname'], user_id, target_group_id, pattern, target_group_pattern)
+            return await __stranger_convert(bot, info['nickname'], target_group_id, pattern, target_group_pattern)
         else:
             traceback.print_exc()
 
 
-async def __stranger_convert(bot: Bot, nickname: str, user_id: int, target_group_id: int, pattern: str, target_group_pattern: str) -> str:
+async def __stranger_convert(
+        bot: Bot,
+        nickname: str,
+        target_group_id: int,
+        pattern: str,
+        target_group_pattern: str
+) -> str:
     group_info = await bot.get_group_info(group_id=target_group_id)
     return pattern.replace('{nickname}', nickname) \
         .replace('{qq_id}', '') \
